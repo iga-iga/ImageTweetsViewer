4fbee20d23e7f475e4c7ec3f54f50950752b0ef3
igaiga <igacode@gmail.com>
2022-02-19 13:22:43 +0900

    add RootTabBar

---
+++ ImageTweetsViewer/Screen/RootTabBar/RootTabBarViewController.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+final class RootTabBarViewController: UITabBarController {
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        self.setViewControllers(
+            [
+            ],
+            animated: false
+        )
+    }
+}
